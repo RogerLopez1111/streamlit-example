17bc6cf1909c5c9a3d1728df064a858ee44ea9ee
roger <rogerlopez1111@gmail.com>
2024-02-19 12:15:30 -0600

    listoo

---
+++ databases.py_correcto.py
@@ -0,0 +1,27 @@
+#mongodb+srv://rogerlopez1111:<password>@prediccion2024.4z5uzg0.mongodb.net/
+import streamlit as st
+import certifi
+import pandas as pd
+from pymongo import MongoClient
+
+st.title("Prueba")
+
+
+def connection():
+    return MongoClient("mongodb+srv://rogerlopez1111:1929394959697989@prediccion2024.4z5uzg0.mongodb.net/", tlsCAFILE=certifi.where())
+
+
+conexion = connection()
+
+
+def getData():
+    db = conexion.get_database("Prediccion")
+    collection = db.get_collection("ejemplo1")
+    items = collection.find()
+    data = [{'_id': str(item['_id']), 'name': item['name'], 'gender': item['gender'],
+             'physics': item['physics'], 'maths': item['maths'], 'english': item['english']} for item in items]
+    return data
+
+
+datos = getData()
+st.dataframe(pd.DataFrame(datos))
